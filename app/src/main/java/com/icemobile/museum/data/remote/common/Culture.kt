7ab412bba46ca97457c01ff370b2e8a74293d9c1
Ali Khoshraftar <alikhoshraftar@live.com>
2021-11-26 08:01:47 +0330

    Added Flavors based on culture. Changed Application icon launcher.

--- app/src/main/java/com/icemobile/museum/data/remote/common/Culture.kt
+++
@@ -1,6 +0,0 @@
-package com.icemobile.museum.data.remote.common
-
-enum class Culture(val code: String) {
-    DUTCH("nl"),
-    ENGLISH("en")
-}
--- app/src/main/java/com/icemobile/museum/data/remote/retrofit/CollectionRetrofitService.kt
+++ app/src/main/java/com/icemobile/museum/data/remote/retrofit/CollectionRetrofitService.kt
@@ -1,6 +1,6 @@
 package com.icemobile.museum.data.remote.retrofit
 
-import com.icemobile.museum.data.remote.common.Culture
+import com.icemobile.museum.BuildConfig
 import com.icemobile.museum.data.remote.model.collection.detail.CollectionDetailDTO
 import com.icemobile.museum.data.remote.model.collection.list.CollectionsDTO
 import retrofit2.http.GET
@@ -8,18 +8,16 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CollectionRetrofitService {
-
     @GET("{culture}/collection")
     suspend fun getCollections(
-        @Path("culture") culture: String = Culture.ENGLISH.code,
+        @Path("culture") culture: String = BuildConfig.Culture,
         @Query("p") page: Int,
         @Query("q") query: String
     ): CollectionsDTO
 
     @GET("{culture}/collection/{object-number}")
     suspend fun getCollectionDetail(
-        @Path("culture") culture: String = Culture.ENGLISH.code,
+        @Path("culture") culture: String = BuildConfig.Culture,
         @Path("object-number") objectNumber: String,
     ): CollectionDetailDTO
-
 }
